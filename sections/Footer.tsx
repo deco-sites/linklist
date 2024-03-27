078eb99dba9ac6661605b20d8e349012d6cdad82
Augusto Barbosa <augustocbarbosa@gmail.com>
2024-03-27 20:25:12 -0300

    Improve sections

--- sections/Footer.tsx
+++ sections/Footer.tsx
@@ -1,29 +1,38 @@
-import { Footer as FooterComponent } from "./types.ts";
+import { ImageWidget } from "apps/admin/widgets.ts";
 import Image from "apps/website/components/Image.tsx";
 import { JSX } from "preact";
 
 interface Props {
-  footer?: FooterComponent;
+  href?: string;
+  image?: ImageWidget;
+  alt?: string;
+  width?: number;
+  height?: number;
+  text?: string;
 }
 
-function Footer(props: Props): JSX.Element | null {
-  const { footer } = props;
-  if (!footer || (!footer.image && !footer.text)) return null;
-
+function Footer({
+  image = "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/4959/d7aa9290-074f-417c-99c3-5b0587c8c2ee",
+  href = "https://deco.cx/",
+  text = "Made with",
+  alt = "Made with deco.cx",
+  height = 20,
+  width = 50,
+}: Props): JSX.Element | null {
   return (
     <div class="py-8 lg:px-0 px-6">
       <a
-        href={footer.url}
+        href={href}
         class="flex flex-row gap-1 items-center justify-center text-xs"
         target="_blank"
       >
-        {footer.text && <p>{footer.text}</p>}
-        {footer.image && (
+        {text && <p>{text}</p>}
+        {image && (
           <Image
-            src={footer.image || ""}
-            alt={footer.alt}
-            width={footer.width || 50}
-            height={footer.height || 20}
+            src={image || ""}
+            alt={alt || ""}
+            height={height || 20}
+            width={width || 50}
           />
         )}
       </a>
--- sections/Image.tsx
+++ sections/Image.tsx
@@ -7,8 +7,11 @@ export interface Picture {
   alt?: string;
 }
 
-export default function Image(props: Picture) {
-  const { mobile, desktop, alt } = props;
+export default function Image({
+  mobile = "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/4959/2bb6e3b9-90e8-49b0-86ce-465d6856343e",
+  desktop = "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/4959/89934286-9a29-4ebc-bc90-39117d2e7edb",
+  alt = "Image",
+}: Picture) {
   return (
     <div class="max-w-[688px] mx-auto py-2 w-full lg:px-0 px-6">
       <figure class="relative">
