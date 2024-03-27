d7d59478416432b367f1d33e29a04522f20a5a87
devartes <anacavalcante731@gmail.com>
2024-03-27 16:13:15 -0300

    feat: add sections

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -4,13 +4,23 @@
 
 import * as $$$$$$$$$$$0 from "./apps/decohub.ts";
 import * as $$$$$$$$$$$1 from "./apps/site.ts";
-import * as $$$$$$0 from "./sections/ItWorks.tsx";
-import * as $$$$$$1 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$0 from "./sections/Footer.tsx";
+import * as $$$$$$1 from "./sections/Header.tsx";
+import * as $$$$$$2 from "./sections/Image.tsx";
+import * as $$$$$$3 from "./sections/Links.tsx";
+import * as $$$$$$4 from "./sections/Shortcuts.tsx";
+import * as $$$$$$5 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$6 from "./sections/types.ts";
 
 const manifest = {
   "sections": {
-    "site/sections/ItWorks.tsx": $$$$$$0,
-    "site/sections/Theme/Theme.tsx": $$$$$$1,
+    "site/sections/Footer.tsx": $$$$$$0,
+    "site/sections/Header.tsx": $$$$$$1,
+    "site/sections/Image.tsx": $$$$$$2,
+    "site/sections/Links.tsx": $$$$$$3,
+    "site/sections/Shortcuts.tsx": $$$$$$4,
+    "site/sections/Theme/Theme.tsx": $$$$$$5,
+    "site/sections/types.ts": $$$$$$6,
   },
   "apps": {
     "site/apps/decohub.ts": $$$$$$$$$$$0,
