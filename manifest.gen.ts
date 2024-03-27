1442363b206ed632a50fe1a9d723a12927f77aa0
Ana Carolina Duarte <ana@deco.cx>
2024-03-27 19:52:27 +0000

    Release Staging environment with 2 changes made by Ana Carolina Duarte

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -2,29 +2,29 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $$$$$$$$$$$0 from "./apps/decohub.ts";
-import * as $$$$$$$$$$$1 from "./apps/site.ts";
-import * as $$$$$$0 from "./sections/Footer.tsx";
-import * as $$$$$$1 from "./sections/Header.tsx";
-import * as $$$$$$2 from "./sections/Image.tsx";
-import * as $$$$$$3 from "./sections/ItWorks.tsx";
-import * as $$$$$$4 from "./sections/Links.tsx";
-import * as $$$$$$5 from "./sections/Theme/Theme.tsx";
-import * as $$$$$$6 from "./sections/types.ts";
+import * as $$$$$$$$$$$$0 from "./apps/decohub.ts";
+import * as $$$$$$$$$$$$1 from "./apps/site.ts";
+import * as $$$$$$$0 from "./sections/Footer.tsx";
+import * as $$$$$$$1 from "./sections/Header.tsx";
+import * as $$$$$$$2 from "./sections/Image.tsx";
+import * as $$$$$$$3 from "./sections/ItWorks.tsx";
+import * as $$$$$$$4 from "./sections/Links.tsx";
+import * as $$$$$$$5 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$$6 from "./sections/types.ts";
 
 const manifest = {
   "sections": {
-    "site/sections/Footer.tsx": $$$$$$0,
-    "site/sections/Header.tsx": $$$$$$1,
-    "site/sections/Image.tsx": $$$$$$2,
-    "site/sections/ItWorks.tsx": $$$$$$3,
-    "site/sections/Links.tsx": $$$$$$4,
-    "site/sections/Theme/Theme.tsx": $$$$$$5,
-    "site/sections/types.ts": $$$$$$6,
+    "site/sections/Footer.tsx": $$$$$$$0,
+    "site/sections/Header.tsx": $$$$$$$1,
+    "site/sections/Image.tsx": $$$$$$$2,
+    "site/sections/ItWorks.tsx": $$$$$$$3,
+    "site/sections/Links.tsx": $$$$$$$4,
+    "site/sections/Theme/Theme.tsx": $$$$$$$5,
+    "site/sections/types.ts": $$$$$$$6,
   },
   "apps": {
-    "site/apps/decohub.ts": $$$$$$$$$$$0,
-    "site/apps/site.ts": $$$$$$$$$$$1,
+    "site/apps/decohub.ts": $$$$$$$$$$$$0,
+    "site/apps/site.ts": $$$$$$$$$$$$1,
   },
   "name": "site",
   "baseUrl": import.meta.url,
